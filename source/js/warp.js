41e2412301ecd5340600d70d7e4fd3b2586f3232
a2396837 <798453240@qq.com>
2020-05-11 15:33:03 +0800

    👍update

--- source/js/warp.js
+++
@@ -1,6 +0,0 @@
-$("img").not('.page img').not('.nav-logo img').each(function () {
-     var element = document.createElement("a");
-     $(element).attr("data-fancybox", "gallery");
-     $(element).attr("href", $(this).attr("src"));
-     $(this).wrap(element);
- });
