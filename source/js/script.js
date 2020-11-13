aeb2047c0841fc890d16c85da8498ea7bbf48671
a2396837 <798453240@qq.com>
2020-11-13 19:28:48 +0800

    fix rollup

--- source/js/script.js
+++ source/js/script.js
@@ -32,7 +32,7 @@
 
 //返回顶部
   window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {console.log(121);
+  function scrollFunction() {
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
           document.getElementById("gotop").style.display = "block";
       } else {
