b1cbbebd198d71c5d1041528fc30bddf160fda68
a2396837 <798453240@qq.com>
2021-01-05 11:50:30 +0800

    🎉修复夜间模式切换白屏，增加Waline评论

--- source/js/script.js
+++ source/js/script.js
@@ -31,18 +31,19 @@
   });
 
 //返回顶部
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById("gotop").style.display = "block";
-      } else {
-          document.getElementById("gotop").style.display = "none";
-      }
-  }
-  function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-  }
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("gotop").style.display = "block";
+    } else {
+        document.getElementById("gotop").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 
 
 
